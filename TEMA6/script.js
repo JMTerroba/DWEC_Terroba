711ce54ac799276bd50b7ada285b8b70beee3b48
JMTerroba <150330679+JMTerroba@users.noreply.github.com>
2024-11-07 13:05:05 +0100

    07/11/2024

--- TEMA6/script.js
+++ TEMA6/script.js
@@ -16,15 +16,48 @@ fetch('http://localhost:3000/habitaciones')
             const row = document.createElement('tr');
 
             row.innerHTML = `
-                    <td>${habitaciones.numero}</td>
-                    <td>${habitaciones.nombre}</td>
-                    <td>${habitaciones.tipo}</td>
-                    <td>${habitaciones.precio}</td>
-                    <td>${habitaciones.fechaDisponibilidad}</td>
-                    <td>${habitaciones.reservada}</td>
-                `;
+            <td>${habitaciones.numero}</td>
+            <td>${habitaciones.nombre}</td>
+            <td>${habitaciones.tipo}</td>
+            <td>${habitaciones.precio}</td>
+            <td>${habitaciones.fechaDisponibilidad}</td>
+            <td>${habitaciones.reservada}</td>
+            <td>
+            ${habitaciones.reservada == true ? '<button>Reservar</button>' : '<button>Liberar</button>'}
+            <button>Eliminar</button>
+            </td>
+            `;
 
             tableBody.appendChild(row);
+        });
+
+        document.getElementById('add-room-form').addEventListener('submit', function(event) {
+            event.preventDefault(); // Evita que el formulario se envíe y recargue la página
+
+            var roomNumber= document.getElementById('room-number').value;
+            var roomName= document.getElementById('room-name').value;
+            var roomType= document.getElementById('room-type').value;
+            var dateDisponibility= document.getElementById('date-disponibility').value;
+
+            data.forEach(habitaciones => {
+                const row = document.createElement('tr');
+    
+                row.innerHTML = `
+                <td>${roomNumber}</td>
+                <td>${roomName}</td>
+                <td>${roomType}</td>
+                <td>${habitaciones.precio}</td>
+                <td>${dateDisponibility}</td>
+                <td>${habitaciones.reservada}</td>
+                <td>
+                ${habitaciones.reservada == true ? '<button>Reservar</button>' : '<button>Liberar</button>'}
+                <button>Eliminar</button>
+                </td>
+                `;
+    
+                tableBody.appendChild(row);
+            });
+
         });
     })
     .catch(error => {
